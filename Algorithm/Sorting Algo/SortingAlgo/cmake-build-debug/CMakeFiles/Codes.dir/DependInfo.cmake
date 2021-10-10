8674899b5ba726b84940f096560ef43ff30e006b
DengSchoo <1425123490@qq.com>
2021-10-10 19:44:42 +0800

    rm Codes dir

--- Algorithm/Sorting Algo/SortingAlgo/cmake-build-debug/CMakeFiles/Codes.dir/DependInfo.cmake
+++ Algorithm/Sorting Algo/SortingAlgo/cmake-build-debug/CMakeFiles/Codes.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "D:/Study/GradeFour/CollegeFourLeaningNotes/Algorithm/Sorting Algo/Codes/main.cpp" "D:/Study/GradeFour/CollegeFourLeaningNotes/Algorithm/Sorting Algo/Codes/cmake-build-debug/CMakeFiles/Codes.dir/main.cpp.obj"
+  "D:/Study/GradeFour/CollegeFourLeaningNotes/Algorithm/Sorting Algo/SortingAlgo/main.cpp" "D:/Study/GradeFour/CollegeFourLeaningNotes/Algorithm/Sorting Algo/SortingAlgo/cmake-build-debug/CMakeFiles/Codes.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
